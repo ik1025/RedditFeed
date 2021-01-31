9323284229dcc33aee2703c7dd306d9026db5a25
ik1025 <ik1025@nyu.edu>
2021-01-31 11:34:22 -0500

    finished functionality

--- subreddit-feed/src/App.js
+++ subreddit-feed/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [subreddit, setSubreddit] = useState('webdev');
 
   useEffect (() => {
-    fetch("https://www.reddit.com/r/WebdevTutorials.json").then( res => {
+    fetch("https://www.reddit.com/r/"+subreddit+".json").then( res => {
       if (res.status != 200) {
         console.log("Error!");
         return;
@@ -26,7 +26,7 @@ function App() {
     <div className="App">
 
       <header className="subreddit_input">
-        <input type="text" className="input" value="webdev"/>
+        <input type="text" className="input" value={subreddit} onChange={ e => setSubreddit(e.target.value)} />
       </header>
 
       <div className="articles">
